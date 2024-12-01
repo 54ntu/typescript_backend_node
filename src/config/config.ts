f751c08d12eb8aef06ee5053be8442b55e7e9e13
54ntu <chaudharysantaram8@gmail.com>
2024-12-01 13:07:58 +0545

    global error handler middleware is created

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 config()
 const envConfig={
     port: process.env.PORT,
-    mongodbstring:process.env.MONGODB_URL
+    mongodbstring:process.env.MONGODB_URL,
+    environment :process.env.NODE_ENV
 }
 
 export default envConfig;
--- src/controllers/note.controllers.ts
+++ src/controllers/note.controllers.ts
@@ -1,15 +1,38 @@
 import { Request, Response } from "express";
 import Note from "../models/note.models";
 
-const createNote= (req:Request,res:Response)=>{
-    const file = req.file
-    const {title,subtitle,description}= req.body
-
-    if(!title || !subtitle || !description){
-        res.status(400).json({
-            message:"all fields are required"
+const createNote=async (req:Request,res:Response)=>{
+    try {
+        const filename = req.file?.filename;
+        const {title,subtitle,description}= req.body
+    
+        if(!title || !subtitle || !description){
+            res.status(400).json({
+                message:"all fields are required"
+            })
+        }
+    
+       const response= await Note.create({title,description,subtitle,file:filename})
+        if(response){
+            return res.status(200).json({message:"note added succcessfully....!!!",data:response})
+    
+    
+        }else{
+            return res.status(500).json({
+                message:"something went wrong...1!!"
+            })
+        }
+    
+    } catch (error) {
+        return res.status(500).json({
+            message:"error while creating notes....."
         })
     }
 
-    Note.create({title,description,subtitle,file})
+
+
 }
+
+
+
+export default createNote;
