2f35b171ee6699acc70d3cf64e303cfd7062c236
54ntu <chaudharysantaram8@gmail.com>
2024-12-01 22:51:05 +0545

    cors is installed and configured

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
+import cors from "cors"
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import noteRoute from "./routes/note.routes";
+import envConfig from "./config/config";
 const app = express();
 
 
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("./src/public"))
+app.use(cors({
+    origin: envConfig.frontend
+}))
 
-app.use("/api/notes",noteRoute) // route for noteroutes
+app.use("/api/notes", noteRoute) // route for noteroutes
 app.use(globalErrorHandler)
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,10 @@
 import { config } from "dotenv";
 config()
-const envConfig={
+const envConfig = {
     port: process.env.PORT,
-    mongodbstring:process.env.MONGODB_URL,
-    environment :process.env.NODE_ENV
+    mongodbstring: process.env.MONGODB_URL,
+    environment: process.env.NODE_ENV,
+    frontend: process.env.FRONTEND_URL
 }
 
 export default envConfig;
